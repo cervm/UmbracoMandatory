678741a7253f683a73448670cac3517348e91748
Marek Cervinka <eaamace@students.eaaa.dk>
2019-10-29 02:10:22 +0100

    Add contact form

---
+++ UmbracoMandatory/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using UmbracoMandatory.ViewModels;
+
+namespace UmbracoMandatory.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            return !ModelState.IsValid ? CurrentUmbracoPage() : (ActionResult)RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ UmbracoMandatory/ViewModels/ContactForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UmbracoMandatory.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [Display(Name = "Email")]
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+            ErrorMessage = "Please enter correct email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a subject")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Please enter a message")]
+        public string Message { get; set; }
+    }
+}
